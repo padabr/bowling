e071bc0cff2434e3fc61920a93bee416b1b549c2
padabr <30824472+padabr@users.noreply.github.com>
2018-02-05 13:03:49 +0100

    Add files via upload

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,11 +1,36 @@
 public class BowlingGame {
+    int last_roll = 0;
+    int frame[][] = new int[10][2];
+    int extra_roll = 0;
 
+   
     public void roll(int pins) {
-
+        last_roll = pins;
     }
 
     public int calculateScore() {
-        return 0;
+        int temp_score = 0;
+        for (int f = 0; f < 8; f++){
+            temp_score = temp_score + frame[f][0] + frame[f][1];
+            if (frame[f][0] + frame[f][1] == 10){
+                temp_score = temp_score + frame[f+1][0];
+            }
+            if (frame[f][0] == 10){
+                temp_score = temp_score + frame[f+1][1];
+                if (frame[f+1][0] == 10){
+                    temp_score = temp_score + frame[f+2][0];
+                }
+            }
+        }
+        temp_score = temp_score + frame[8][0] + frame[8][1];
+        if ((frame[8][0] + frame[8][1] == 10)) {
+            temp_score = temp_score + frame[9][0];
+        }
+        if (frame[8][0] == 10){
+            temp_score = temp_score + frame[9][1];
+        }
+        temp_score = temp_score + frame[9][0] + frame[9][1] + extra_roll;
+        return temp_score;
     }
 
 }
