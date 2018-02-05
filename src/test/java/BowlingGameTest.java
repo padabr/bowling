993e10059c77961bebc270e80166071ee2ac1a11
padabr <30824472+padabr@users.noreply.github.com>
2018-02-05 12:36:29 +0100

    Add files via upload

--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -10,33 +10,43 @@ public class BowlingGameTest {
         assertEquals(0, game.calculateScore());
     }
     
+    @Test
     public void bowlingPerfectGameIs300() {
         BowlingGame game = new BowlingGame();
-        for (int f = 1; f < 10; f++){
-            game.frame[f][1] = 10;
-            game.frame[f][2] = 0;
+        for (int f = 0; f < 9; f++){
+            game.frame[f][0] = 10;
+            game.frame[f][1] = 0;
         }
-        game.frame[10][1] = 10;
-        game.frame[10][2] = 10;
+        game.frame[9][0] = 10;
+        game.frame[9][1] = 10;
         game.extra_roll = 10;
         assertEquals(300, game.calculateScore());
     }
 
+    @Test
     public void bowlingGivenGameIs155() {
         BowlingGame game = new BowlingGame();
-        game.frame[][] = {
-            {3, 7}
-            {1, 6}
-            {10, 0}
-            {10, 0}
-            {7, 3}
-            {8, 0}
-            {5, 2}
-            {0, 7}
-            {9, 1}
-            {10, 10}
-        }
-        game.extra_roll = 10;
+        game.frame[0][0] = 3;
+        game.frame[0][1] = 7;
+        game.frame[1][0] = 1;
+        game.frame[1][1] = 6;
+        game.frame[2][0] = 10;
+        game.frame[2][1] = 0;
+        game.frame[3][0] = 10;
+        game.frame[3][1] = 0;
+        game.frame[4][0] = 7;
+        game.frame[4][1] = 3;
+        game.frame[5][0] = 8;
+        game.frame[5][1] = 0;        
+        game.frame[6][0] = 5;
+        game.frame[6][1] = 2;
+        game.frame[7][0] = 0;
+        game.frame[7][1] = 7;
+        game.frame[8][0] = 9;
+        game.frame[8][1] = 1;
+        game.frame[9][0] = 10;
+        game.frame[9][1] = 10;
+		game.extra_roll = 10;
         assertEquals(155, game.calculateScore());
     }
 
