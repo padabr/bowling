5608c4da7915a1e0f26dc738c21c93d4f497f83d
padabr <30824472+padabr@users.noreply.github.com>
2018-02-07 15:32:14 +0100

    Add files via upload

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,35 +1,77 @@
 public class BowlingGame {
-    int last_roll = 0;
-    int frame[][] = new int[10][2];
-    int extra_roll = 0;
+
+    private int last_roll = 0;
+    private int frame[][] = new int[10][2];
+    private int f, f_roll;
+    private int extra_roll = 0;
+    private boolean gameover = false;
+    private boolean bExtraRoll = false;
+
+    private void actualizeFrame(){
+        if (gameover == false) {
+            if (bExtraRoll == true){
+                extra_roll = last_roll; // <--- if extra roll
+                gameover = true;
+            }else{
+                frame[f][f_roll] = last_roll; // <---- here to actualize score table <---
+                if (f == 9){                //last frame - beginning
+                    if (f_roll == 0){           // the first roll in the last frame
+                        f_roll = 1;
+                    }else{                     // the second roll in the last frame            
+                        if ((frame[f][0] == 10) || (frame[f][0] + frame[f][1] == 10)){ // l_f and the second strike
+                            bExtraRoll = true;
+                        }else{
+                            gameover = true;
+                        }
+                    }      
+                }else{                      // betw frame - beginning
+                    if (f_roll == 0){       // the first roll in frame
+                        if (frame[f][0] == 10){ // betw frame - strike
+                            f = f + 1;
+                        }else{             // the second roll in frame
+                            f_roll = 1;
+                        }
+                    }else{
+                        f = f + 1;
+                        f_roll = 0;
+                    }
+                }
+            }      // not an extra roll
+        }    // not game over
+    }
     
     public void roll(int pins) {
         last_roll = pins;
+//        System.out.println("last roll = " + last_roll);
+        actualizeFrame();
     }
 
     public int calculateScore() {
         int temp_score = 0;
-        for (int f = 1; f < 9; f++){
-            temp_score = temp_score + frame[f][1] + frame[f][2];
-            if (frame[f][1] + frame[f][2] == 10){
-                temp_score = temp_score + frame[f+1][1];
+        
+        for (int f = 0; f < 8; f++){
+            temp_score = temp_score + frame[f][0] + frame[f][1];
+            if (frame[f][0] + frame[f][1] == 10){
+                temp_score = temp_score + frame[f+1][0];
             }
-            if (frame[f][1] == 10){
-                temp_score = temp_score + frame[f+1][2];
-                if (frame[f+1][1] == 10){
-                    temp_score = temp_score + frame[f+2][1];
+            if (frame[f][0] == 10){
+                temp_score = temp_score + frame[f+1][1];
+                if (frame[f+1][0] == 10){
+                    temp_score = temp_score + frame[f+2][0];
                 }
             }
         }
-        temp_score = temp_score + frame[9][1] + frame[9][2];
-        if ((frame[9][1] + frame[9][2] == 10)) {
-            temp_score = temp_score + frame[10][1];
+        temp_score = temp_score + frame[8][0] + frame[8][1];
+        if ((frame[8][0] + frame[8][1] == 10)) {
+            temp_score = temp_score + frame[9][0];
         }
-        if (frame[9][1] == 10){
-            temp_score = temp_score + frame[10][2];
+        if (frame[8][0] == 10){
+            temp_score = temp_score + frame[9][1];
         }
-        temp_score = temp_score + frame[10][1] + frame[10][2] + extra_roll;
+        temp_score = temp_score + frame[9][0] + frame[9][1] + extra_roll;
         return temp_score;
     }
 
+    
 }
+
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -13,41 +13,44 @@ public class BowlingGameTest {
     @Test
     public void bowlingPerfectGameIs300() {
         BowlingGame game = new BowlingGame();
-        for (int f = 0; f < 9; f++){
-            game.frame[f][0] = 10;
-            game.frame[f][1] = 0;
-        }
-        game.frame[9][0] = 10;
-        game.frame[9][1] = 10;
-        game.extra_roll = 10;
+        game.roll(10);
+        game.roll(10);
+        game.roll(10);
+        game.roll(10);
+        game.roll(10);
+        game.roll(10);
+        game.roll(10);
+        game.roll(10);
+        game.roll(10);
+        game.roll(10);
+        game.roll(10);
+        game.roll(10);
         assertEquals(300, game.calculateScore());
     }
 
     @Test
     public void bowlingGivenGameIs155() {
         BowlingGame game = new BowlingGame();
-        game.frame[0][0] = 3;
-        game.frame[0][1] = 7;
-        game.frame[1][0] = 1;
-        game.frame[1][1] = 6;
-        game.frame[2][0] = 10;
-        game.frame[2][1] = 0;
-        game.frame[3][0] = 10;
-        game.frame[3][1] = 0;
-        game.frame[4][0] = 7;
-        game.frame[4][1] = 3;
-        game.frame[5][0] = 8;
-        game.frame[5][1] = 0;        
-        game.frame[6][0] = 5;
-        game.frame[6][1] = 2;
-        game.frame[7][0] = 0;
-        game.frame[7][1] = 7;
-        game.frame[8][0] = 9;
-        game.frame[8][1] = 1;
-        game.frame[9][0] = 10;
-        game.frame[9][1] = 10;
-		game.extra_roll = 10;
-        assertEquals(155, game.calculateScore());
+        game.roll(3);
+        game.roll(7);
+        game.roll(1);
+        game.roll(6);
+        game.roll(10);
+        game.roll(10);
+        game.roll(7);
+        game.roll(3);
+        game.roll(8);
+        game.roll(0);
+        game.roll(5);
+        game.roll(2);
+        game.roll(0);
+        game.roll(7);
+        game.roll(9);
+        game.roll(1);
+        game.roll(10);
+        game.roll(10);
+        game.roll(10);
+		assertEquals(155, game.calculateScore());
     }
 
 }
